84a9885da36ed11b3078e42d6bca3afd2e739e23
Jan Sandbrink <jan.sandbrink@kaeuferportal.de>
2018-07-18 11:26:09 +0200

    Link files during setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+export DOTFILES=`dirname "$0"`
+export DOTFILES=`readlink -f "$DOTFILES"`
+
 # rbenv
 if [ ! -d "$HOME/.rbenv" ]; then
   echo "Installing RVM..."
@@ -9,3 +12,17 @@ if [ ! -d "$HOME/.rbenv" ]; then
   mkdir -p ~/.rbenv/plugins
   $ git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 fi
+
+function link_file {
+  if [ ! -L "$HOME/$1" ]; then
+    echo "Creating $1..."
+    [ -e "$HOME/$1" ] && rm "$HOME/$1"
+    ln -s "$DOTFILES/$1" "$HOME/$1"
+  fi
+}
+
+link_file ".bashrc"
+link_file ".gemrc"
+link_file ".gitconfig"
+link_file ".gitignore"
+link_file ".vimrc"
