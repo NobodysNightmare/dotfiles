4187ec5dab8852ed13b51df3530a28e2712c860c
Jan Sandbrink <jan.sandbrink@kaeuferportal.de>
2018-07-18 15:40:59 +0200

    Fix another typo and add more plugins

--- setup.sh
+++ setup.sh
@@ -9,7 +9,9 @@ if [ ! -d "$HOME/.rbenv" ]; then
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
   cd ~/.rbenv && src/configure && make -C src
   mkdir -p ~/.rbenv/plugins
-  $ git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  git clone https://github.com/rbenv/rbenv-vars.git ~/.rbenv/plugins/rbenv-vars
+  git clone https://github.com/rbenv/rbenv-each.git ~/.rbenv/plugins/rbenv-each
 fi
 
 function link_file {
