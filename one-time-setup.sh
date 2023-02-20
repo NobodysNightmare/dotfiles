115e6c6f03c8389f9b7ed262b0671f662f849440
Jan Sandbrink <nobodysnightmare@gmail.com>
2023-02-20 16:43:23 +0100

    Start documenting random apt-installs

---
+++ one-time-setup.sh
@@ -0,0 +1,7 @@
+# Bunch of things that I usually want to do on a new machine
+# as well, but that does not yet have a "repeat-protection".
+
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo apt install -y git
+
+sudo apt install -y docker.io
