c3e9628efd10a8e571f1fcb30dffeb370b6082f4
Jan Sandbrink <nobodysnightmare@gmail.com>
2023-09-08 12:47:14 +0200

    Adapt one time installer
    Document more necessary stuff and remove docker
    for now, because recommended installation is not
    using the docker.io package.

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -1,7 +1,9 @@
 # Bunch of things that I usually want to do on a new machine
 # as well, but that does not yet have a "repeat-protection".
 
+echo "Using more up-to-date Git..."
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt install -y git
 
-sudo apt install -y docker.io
+echo "To enable Wayland-support for Firefox, add this to /etc/environment:"
+echo "    MOZ_ENABLE_WAYLAND=1"
